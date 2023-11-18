9e8299fc0bcf417930b761fec4643ca3b85889e6
dev-msj <msj944@naver.com>
2023-11-18 21:27:18 +0900

    feat(app.module): winston 모듈 설정

--- personal-blog-service/src/app.module.ts
+++ personal-blog-service/src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeOrmConfig';
+import { WinstonModule, utilities } from 'nest-winston';
+import winston from 'winston';
 
 @Module({
   imports: [
@@ -12,6 +14,17 @@ import { typeOrmConfig } from './config/typeOrmConfig';
       envFilePath: `env/.${process.env.NODE_ENV}.env`,
     }),
     TypeOrmModule.forRootAsync(typeOrmConfig),
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            utilities.format.nestLike('PersonalBlog', { prettyPrint: true }),
+          ),
+        }),
+      ],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
