ec304462c92f7343fad8b115fa5cdee5daae8f73
dev-msj <msj944@naver.com>
2023-11-18 21:17:12 +0900

    feat(app.module): typeOrmConfig 추가

--- personal-blog-service/src/app.module.ts
+++ personal-blog-service/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeOrmConfig';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `env/.${process.env.NODE_ENV}.env`,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
