882e9d9e18d2b40ec5d739a4a7b8551959b5fa32
dev-msj <msj944@naver.com>
2023-11-22 02:48:08 +0900

    feat: PostDto 추가

---
+++ personal-blog-service/src/blog/dto/post.dto.ts
@@ -0,0 +1,8 @@
+export class PostDto {
+  readonly postUid: string;
+  readonly postId: number;
+  readonly title: string;
+  readonly wrtieDatetime: Date;
+  readonly contents: string;
+  readonly hits: number;
+}
