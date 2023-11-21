458a3d5e060f53b6738c8f175fb72410436a94ee
dev-msj <msj944@naver.com>
2023-11-22 02:49:09 +0900

    feat: PostDao 추가
    PostEntity를 PostDao로 변경하는 메서드 구현
    PostDao를 PostDto로 변경하는 메서드 구현

---
+++ personal-blog-service/src/blog/dao/post.dao.ts
@@ -0,0 +1,32 @@
+import { PostDto } from '../dto/post.dto';
+import { PostEntity } from '../entities/post.entity';
+
+export class PostDao {
+  private postUid: string;
+  private postId: number;
+  private title: string;
+  private wrtieDatetime: Date;
+  private contents: string;
+  private hits: number;
+
+  static fromPostEntity(postEntity: PostEntity): PostDao {
+    const postDao = new PostDao();
+    postDao.postUid = postEntity.postUid;
+    postDao.postId = postEntity.postId;
+    postDao.wrtieDatetime = postEntity.wrtieDatetime;
+    postDao.contents = postEntity.contents;
+    postDao.hits = postEntity.hits;
+
+    return postDao;
+  }
+
+  toPostDto(): PostDto {
+    return {
+      postUid: this.postUid,
+      postId: this.postId,
+      wrtieDatetime: this.wrtieDatetime,
+      contents: this.contents,
+      hits: this.hits,
+    } as PostDto;
+  }
+}
