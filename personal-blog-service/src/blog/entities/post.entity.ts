363ac76323f0f4da1baf66a474e735d2403a6ac3
dev-msj <msj944@naver.com>
2023-11-22 02:46:51 +0900

    refactor(post.entity): 컬럼 수정 및 relation 정의
    title, createDatetime, modifyDatetime 컬럼 추가
    
    datetime 관련 컬럼의 데코레이터를
    CreateDateColumn/UpdateDateColumn으로 변경
    
    PostLikeEntity와의 관계를 OneToMany로 정의

--- personal-blog-service/src/blog/entities/post.entity.ts
+++ personal-blog-service/src/blog/entities/post.entity.ts
@@ -1,4 +1,13 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { PostLikeEntity } from './post-like.entity';
 
 @Entity('POST')
 export class PostEntity {
@@ -8,12 +17,33 @@ export class PostEntity {
   @PrimaryColumn({ name: 'POST_ID' })
   readonly postId: number;
 
-  @Column({ name: 'WRITE_DATE', type: 'datetime' })
-  readonly wrtieDate: Date;
+  @Column({ name: 'TITLE', length: 500 })
+  readonly title: string;
+
+  @CreateDateColumn({ name: 'WRITE_DATETIME', type: 'datetime' })
+  readonly wrtieDatetime: Date;
 
   @Column({ name: 'CONTENTS', type: 'text' })
   readonly contents: string;
 
   @Column({ name: 'HITS' })
   readonly hits: number;
+
+  @CreateDateColumn({
+    name: 'CREATE_DATETIME',
+    type: 'datetime',
+  })
+  readonly createDatetime: Date;
+
+  @UpdateDateColumn({
+    name: 'MODIFY_DATETIME',
+    type: 'datetime',
+  })
+  readonly modifyDatetime: Date;
+
+  @OneToMany(
+    () => PostLikeEntity,
+    (postLikeEntity) => postLikeEntity.postEntity,
+  )
+  readonly postLikeEntitys: PostLikeEntity;
 }
