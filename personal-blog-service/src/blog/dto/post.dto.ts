60631d282c718b9c83c70c8bf51eb797b6100a9b
dev-msj <msj944@naver.com>
2023-11-22 02:53:26 +0900

    refactor(post.dto): type validation 추가

--- personal-blog-service/src/blog/dto/post.dto.ts
+++ personal-blog-service/src/blog/dto/post.dto.ts
@@ -1,8 +1,21 @@
+import { IsDate, IsNumber, IsString } from 'class-validator';
+
 export class PostDto {
+  @IsString()
   readonly postUid: string;
+
+  @IsNumber()
   readonly postId: number;
+
+  @IsString()
   readonly title: string;
+
+  @IsDate()
   readonly wrtieDatetime: Date;
+
+  @IsString()
   readonly contents: string;
+
+  @IsNumber()
   readonly hits: number;
 }
