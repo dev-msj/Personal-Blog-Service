55abbe130dcdc657459886069ec11af3fe4f4503
dev-msj <msj944@naver.com>
2023-11-24 00:41:47 +0900

    feat(blog.module): providers에 PostLikeService 추가

--- personal-blog-service/src/blog/blog.module.ts
+++ personal-blog-service/src/blog/blog.module.ts
@@ -4,10 +4,11 @@ import { PostEntity } from './entities/post.entity';
 import { PostController } from './controller/post.controller';
 import { PostService } from './service/post.serivce';
 import { PostLikeEntity } from './entities/post-like.entity';
+import { PostLikeService } from './service/post-like.serivce';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PostEntity, PostLikeEntity])],
   controllers: [PostController],
-  providers: [PostService],
+  providers: [PostService, PostLikeService],
 })
 export class BlogModule {}
