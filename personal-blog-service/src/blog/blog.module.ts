ab2ca5e0a9d230219e6707026e904972d9f670df
dev-msj <msj944@naver.com>
2023-11-24 17:48:15 +0900

    feat(blog.module): UserInfoService 연동

--- personal-blog-service/src/blog/blog.module.ts
+++ personal-blog-service/src/blog/blog.module.ts
@@ -5,10 +5,14 @@ import { PostController } from './controller/post.controller';
 import { PostService } from './service/post.serivce';
 import { PostLikeEntity } from './entities/post-like.entity';
 import { PostLikeService } from './service/post-like.serivce';
+import { UserInfoService } from 'src/user/service/user-info.service';
+import { UserInfoEntity } from 'src/user/entities/user-info.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostEntity, PostLikeEntity])],
+  imports: [
+    TypeOrmModule.forFeature([PostEntity, PostLikeEntity, UserInfoEntity]),
+  ],
   controllers: [PostController],
-  providers: [PostService, PostLikeService],
+  providers: [PostService, PostLikeService, UserInfoService],
 })
 export class BlogModule {}
