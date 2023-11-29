98471139a7573136fbfc47aa68b4db1ec1669369
dev-msj <msj944@naver.com>
2023-11-29 16:45:09 +0900

    refactor(typeOrmConfig): 쿼리 결과를 redis에 캐싱할 수 있도록 설정

--- personal-blog-service/src/config/typeOrmConfig.ts
+++ personal-blog-service/src/config/typeOrmConfig.ts
@@ -1,5 +1,6 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { TimeUtils } from 'src/utils/time.utills';
 
 export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
@@ -14,6 +15,15 @@ export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
       database: configService.get('DB_DATABASE'),
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: configService.get('DB_SYNCHRONIZE') === 'true',
+      cache: {
+        type: 'redis',
+        duration: TimeUtils.getTicTimeHMS(24),
+        options: {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT'),
+        },
+        ignoreErrors: true,
+      },
     };
   },
 };
