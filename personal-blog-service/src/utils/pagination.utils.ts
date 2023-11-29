9b53949e8a2f40cc318734803a20d4cdc85cf503
dev-msj <msj944@naver.com>
2023-11-30 00:17:29 +0900

    feat: PaginationUtils 구현
    한번에 가져올 데이터의 개수 take 정의
    PaginationDto를 생성하는 toPaginationDto 메서드 구현

---
+++ personal-blog-service/src/utils/pagination.utils.ts
@@ -0,0 +1,20 @@
+import { PaginationDto } from '../blog/dto/pagination.dto';
+import { PaginationMetaDto } from '../blog/dto/pagination-meta.dto';
+
+export class PaginationUtils {
+  static readonly TAKE: number = 2;
+
+  static toPaginationDto<T>(data: T[], total: number, currentPage: number) {
+    const paginationMetaDto = new PaginationMetaDto(
+      total,
+      currentPage,
+      this.getLastPage(total),
+    );
+
+    return new PaginationDto(data, paginationMetaDto);
+  }
+
+  private static getLastPage(total: number) {
+    return Math.ceil(total / this.TAKE);
+  }
+}
