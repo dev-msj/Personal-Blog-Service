eafca9d54b57ed823cb14ecf8626fe609ca5dfe1
dev-msj <msj944@naver.com>
2023-11-30 16:14:17 +0900

    refactor(post.service): repository 의존성 분리
    TypeORM 관련 로직들을 Repository 레이어로 분리

--- personal-blog-service/src/blog/service/post.serivce.ts
+++ personal-blog-service/src/blog/service/post.serivce.ts
@@ -1,7 +1,4 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { PostEntity } from '../entities/post.entity';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
 import { PostDto } from '../dto/post.dto';
 import { PostDao } from '../dao/post.dao';
 import { PostLikeService } from './post-like.serivce';
@@ -9,14 +6,14 @@ import authConfig from 'src/config/authConfig';
 import { ConfigType } from '@nestjs/config';
 import { PaginationDto } from '../dto/pagination.dto';
 import { PaginationUtils } from 'src/utils/pagination.utils';
+import { PostRepository } from '../repository/post.repository';
 
 @Injectable()
 export class PostService {
   constructor(
     @Inject(authConfig.KEY)
     private config: ConfigType<typeof authConfig>,
-    @InjectRepository(PostEntity)
-    private readonly postRrepository: Repository<PostEntity>,
+    private readonly postRepository: PostRepository,
     private readonly postLikeService: PostLikeService,
   ) {}
 
@@ -24,16 +21,8 @@ export class PostService {
     authUid: string,
     page: number = 1,
   ): Promise<PaginationDto<PostDto>> {
-    const [postEntityList, total] = await this.postRrepository.findAndCount({
-      where: { postUid: authUid },
-      take: PaginationUtils.TAKE,
-      skip: (page - 1) * PaginationUtils.TAKE,
-      order: { postId: 'DESC' },
-    });
-
-    const postDaoList = postEntityList.map((postEntity) =>
-      PostDao.fromPostEntity(postEntity),
-    );
+    const [postDaoList, total] =
+      await this.postRepository.findPostDaoListAndCount(authUid, page);
 
     await this.setPostLikeUidList(postDaoList);
 
@@ -41,18 +30,6 @@ export class PostService {
       postDaoList.map((postDao) => postDao.toPostDto(this.config.pkSecretKey)),
       total,
       page,
-    );
-  }
-
-  private async getMaxPostId(authUid: string): Promise<number> {
-    return (
-      (
-        await this.postRrepository
-          .createQueryBuilder('postEntity')
-          .select('MAX(postEntity.postId)', 'max')
-          .where('postEntity.postUid = :postUid', { postUid: authUid })
-          .getRawOne()
-      ).max || 0
     );
   }
 
