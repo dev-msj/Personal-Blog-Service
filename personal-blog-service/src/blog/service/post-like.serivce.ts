64b29e763d65cde94baf66c9b2de0ee8c0e37582
dev-msj <msj944@naver.com>
2023-11-30 16:43:15 +0900

    refactor(post-like.service): repository 의존성 분리
    TypeORM 관련 로직들을 Repository 레이어로 분리

--- personal-blog-service/src/blog/service/post-like.serivce.ts
+++ personal-blog-service/src/blog/service/post-like.serivce.ts
@@ -1,20 +1,17 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
-import { PostLikeEntity } from '../entities/post-like.entity';
 import { UserInfoService } from 'src/user/service/user-info.service';
 import { PostLikeDto } from '../dto/post-like.dto';
-import { PostLikeDao } from '../dao/post-like.dao';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
+import { PostLikeRepository } from '../repository/post-like.repository';
+import { PostLikeDao } from '../dao/post-like.dao';
 
 @Injectable()
 export class PostLikeService {
   constructor(
     @Inject(WINSTON_MODULE_PROVIDER)
     private readonly logger: Logger,
-    @InjectRepository(PostLikeEntity)
-    private readonly postLikeRrepository: Repository<PostLikeEntity>,
+    private readonly postLikeRepository: PostLikeRepository,
     private readonly userInfoService: UserInfoService,
   ) {}
 
@@ -22,35 +19,32 @@ export class PostLikeService {
     postUid: string,
     postId: number,
   ): Promise<string[]> {
-    const postLikeEntityList = await this.postLikeRrepository.find({
-      where: { postUid: postUid, postId: postId },
-    });
+    const postLikeDaoList = await this.postLikeRepository.findPostLikeDaoList(
+      postUid,
+      postId,
+    );
 
-    return this.getNicknameList(postLikeEntityList);
+    return this.getNicknameList(postLikeDaoList);
   }
 
-  private async getNicknameList(postLikeEntityList: PostLikeEntity[]) {
+  async addPostLikeUser(postLikeDto: PostLikeDto) {
+    await this.postLikeRepository.savePostLikeDto(postLikeDto);
+  }
+
+  async removePostLikeUser(postLikeDto: PostLikeDto) {
+    await this.postLikeRepository.removePostLikeDto(postLikeDto);
+  }
+
+  private async getNicknameList(postLikeDaoList: PostLikeDao[]) {
     const nicknameList = [];
-    for (const userInfoEntity of postLikeEntityList) {
+    for (const postLikeDao of postLikeDaoList) {
       const userInfo = await this.userInfoService.getUserInfoDto(
-        userInfoEntity.uid,
+        postLikeDao.getUid,
       );
 
       nicknameList.push(userInfo.nickname);
     }
 
     return nicknameList;
-  }
-
-  async addPostLikeUser(postLikeDto: PostLikeDto) {
-    await this.postLikeRrepository.save(
-      PostLikeDao.fromPostLikeDto(postLikeDto).toPostLikeEntity(),
-    );
-  }
-
-  async removePostLikeUser(postLikeDto: PostLikeDto) {
-    await this.postLikeRrepository.remove(
-      PostLikeDao.fromPostLikeDto(postLikeDto).toPostLikeEntity(),
-    );
   }
 }
