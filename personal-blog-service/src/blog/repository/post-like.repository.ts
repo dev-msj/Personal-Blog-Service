054331b2d2898765ee7763329dd4bd032c1d32b4
dev-msj <msj944@naver.com>
2023-11-30 17:48:04 +0900

    refactor(post-like.service): redis 캐싱 적용
    read 관련 메서드에서는 캐시 생성,
    create/update/delete 관련 메서드에서는 캐시를 삭제하도록 함

--- personal-blog-service/src/blog/repository/post-like.repository.ts
+++ personal-blog-service/src/blog/repository/post-like.repository.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { PostLikeEntity } from '../entities/post-like.entity';
 import { PostLikeDao } from '../dao/post-like.dao';
 import { PostLikeDto } from '../dto/post-like.dto';
+import { TimeUtils } from 'src/utils/time.utills';
+import { CacheIdUtils } from 'src/utils/cache-id.utils';
 
 @Injectable()
 export class PostLikeRepository {
   constructor(
     @InjectRepository(PostLikeEntity)
     private readonly postLikeRepository: Repository<PostLikeEntity>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async findPostLikeDaoList(
@@ -19,19 +22,39 @@ export class PostLikeRepository {
     return (
       await this.postLikeRepository.find({
         where: { postUid: postUid, postId: postId },
+        cache: {
+          id: CacheIdUtils.getPostLikeEntityListCacheId(postUid, postId),
+          milliseconds: TimeUtils.getTicTimeHMS(24),
+        },
       })
     ).map((postLikeEntiy) => PostLikeDao.fromPostLikeEntity(postLikeEntiy));
   }
 
   async savePostLikeDto(postLikeDto: PostLikeDto): Promise<void> {
+    await this.removePostLikeEntityListCache(
+      postLikeDto.postUid,
+      postLikeDto.postId,
+    );
+
     await this.postLikeRepository.save(
       PostLikeDao.fromPostLikeDto(postLikeDto).toPostLikeEntity(),
     );
   }
 
   async removePostLikeDto(postLikeDto: PostLikeDto): Promise<void> {
+    await this.removePostLikeEntityListCache(
+      postLikeDto.postUid,
+      postLikeDto.postId,
+    );
+
     await this.postLikeRepository.remove(
       PostLikeDao.fromPostLikeDto(postLikeDto).toPostLikeEntity(),
     );
+  }
+
+  private async removePostLikeEntityListCache(postUid: string, postId: number) {
+    await this.dataSource.queryResultCache.remove([
+      CacheIdUtils.getPostLikeEntityListCacheId(postUid, postId),
+    ]);
   }
 }
