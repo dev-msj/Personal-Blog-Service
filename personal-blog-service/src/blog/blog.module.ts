6cca8afeb9d05f220752004fd988d5c9a34cc5bd
dev-msj <msj944@naver.com>
2023-11-30 16:50:47 +0900

    refactor(blog.module): 의존성 모듈 추가

--- personal-blog-service/src/blog/blog.module.ts
+++ personal-blog-service/src/blog/blog.module.ts
@@ -5,14 +5,17 @@ import { PostController } from './controller/post.controller';
 import { PostService } from './service/post.serivce';
 import { PostLikeEntity } from './entities/post-like.entity';
 import { PostLikeService } from './service/post-like.serivce';
-import { UserInfoService } from 'src/user/service/user-info.service';
 import { UserInfoEntity } from 'src/user/entities/user-info.entity';
+import { PostRepository } from './repository/post.repository';
+import { PostLikeRepository } from './repository/post-like.repository';
+import { ExportUserModule } from 'src/user/export/export-user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([PostEntity, PostLikeEntity, UserInfoEntity]),
+    ExportUserModule,
   ],
   controllers: [PostController],
-  providers: [PostService, PostLikeService, UserInfoService],
+  providers: [PostService, PostLikeService, PostRepository, PostLikeRepository],
 })
 export class BlogModule {}
