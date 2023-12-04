c4bbe00a8cdb56499fd0d549d5669126cdfbd780
dev-msj <msj944@naver.com>
2023-12-05 03:33:35 +0900

    feat: JWT 관련 env 추가

--- personal-blog-service/src/config/authConfig.ts
+++ personal-blog-service/src/config/authConfig.ts
@@ -2,4 +2,9 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('config', () => ({
   pkSecretKey: process.env.PK_SECRET_KEY,
+  jwtSecretKey: process.env.JWT_SECRET_KEY,
+  jwtIssuer: process.env.JWT_ISSUER,
+  accessTokenExpireTime: process.env.JWT_ACCESSTOKEN_EXPIRE_TIME,
+  refreshTokenExpireTime: process.env.JWT_REFRESHTOKEN_EXPIRE_TIME,
+  refreshTokenReissueTime: process.env.JWT_REFRESHTOKEN_REISSUE_TIME,
 }));
--- personal-blog-service/src/config/validationEnv.ts
+++ personal-blog-service/src/config/validationEnv.ts
@@ -2,4 +2,9 @@ import * as Joi from 'joi';
 
 export const validationEnv = Joi.object({
   PK_SECRET_KEY: Joi.string().required().length(16),
+  JWT_SECRET_KEY: Joi.string().required(),
+  JWT_ISSUER: Joi.string().required(),
+  JWT_ACCESSTOKEN_EXPIRE_TIME: Joi.string().required(),
+  JWT_REFRESHTOKEN_EXPIRE_TIME: Joi.string().required(),
+  JWT_REFRESHTOKEN_REISSUE_TIME: Joi.number().required(),
 });
