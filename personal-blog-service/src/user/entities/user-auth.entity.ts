9d5f7a99aef4c5ecaa673b27ca7f19493ce1629d
dev-msj <msj944@naver.com>
2023-12-05 04:47:56 +0900

    feat: UserAuthEntity 구현

---
+++ personal-blog-service/src/user/entities/user-auth.entity.ts
@@ -0,0 +1,58 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  OneToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { UserRole } from '../../constant/user-role.enum';
+import { UserInfoEntity } from './user-info.entity';
+import { PostLikeEntity } from '../../blog/entities/post-like.entity';
+import { PostEntity } from '../../blog/entities/post.entity';
+
+@Entity('USER_AUTH')
+export class UserAuthEntity {
+  @PrimaryColumn({ name: 'UID', length: 100 })
+  readonly uid: string;
+
+  @Column({ name: 'PASSWORD', length: 1000 })
+  readonly password: string;
+
+  @Column({ name: 'SOCIAL_YN', type: 'char', length: 1 })
+  readonly socialYN: string;
+
+  @Column({ name: 'REFRESH_TOKEN', length: 500 })
+  readonly refreshToken: string;
+
+  @Column({ name: 'USER_ROLE', length: 50 })
+  readonly userRole: UserRole;
+
+  @CreateDateColumn({
+    name: 'CREATE_DATETIME',
+    type: 'datetime',
+  })
+  readonly createDatetime: Date;
+
+  @UpdateDateColumn({
+    name: 'MODIFY_DATETIME',
+    type: 'datetime',
+  })
+  readonly modifyDatetime: Date;
+
+  @OneToOne(() => UserInfoEntity, (userInfoEntity) => userInfoEntity.uid, {
+    onDelete: 'CASCADE',
+  })
+  readonly userInfo: UserInfoEntity;
+
+  @OneToMany(() => PostEntity, (postEntity) => postEntity.postUid, {
+    onDelete: 'CASCADE',
+  })
+  readonly postEntitys: PostEntity[];
+
+  @OneToMany(() => PostLikeEntity, (postLikeEntity) => postLikeEntity.uid, {
+    onDelete: 'CASCADE',
+  })
+  readonly postLikeEntitys: PostLikeEntity[];
+}
