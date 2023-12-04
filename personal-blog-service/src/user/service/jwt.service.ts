15aa013c3b74fb6e1bb20b2d5cb24efcaa6a55ff
dev-msj <msj944@naver.com>
2023-12-05 07:56:16 +0900

    feat(jwt.service): 토큰 검증 및 재발행 구현
    access token을 검증하는 verifyAccessToken 메서드 구현
    refresh token을 검증하는 verifyRefreshToken 메서드 구현
    refresh token의 expire를 체크하여 토큰을 재발행하는
    reissueJwtByUserSessionDto 메서드 구현

--- personal-blog-service/src/user/service/jwt.service.ts
+++ personal-blog-service/src/user/service/jwt.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
 import { ConfigType } from '@nestjs/config';
 import { JwtDto } from '../dto/jwt.dto';
@@ -7,6 +7,8 @@ import authConfig from '../../config/authConfig';
 import { UserRole } from '../../constant/user-role.enum';
 import { UserAuthRepository } from '../repository/user-auth.repository';
 import { UserSessionDto } from '../dto/user-session.dto';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 @Injectable()
 export class JwtService {
@@ -16,6 +18,8 @@ export class JwtService {
   constructor(
     @Inject(authConfig.KEY)
     private readonly config: ConfigType<typeof authConfig>,
+    @Inject(WINSTON_MODULE_PROVIDER)
+    private readonly logger: Logger,
     private readonly userAuthRepository: UserAuthRepository,
   ) {}
 
@@ -28,6 +32,75 @@ export class JwtService {
     );
 
     return new JwtDto(accessToken, refreshToken);
+  }
+
+  async verifyAccessToken(accessToken: string): Promise<void> {
+    try {
+      const decodedJwt = this.decodeToken(accessToken);
+
+      await this.userAuthRepository.getUserSessionDtoByUid(
+        CryptoUtils.decryptPostPK(decodedJwt['uid'], this.config.pkSecretKey),
+      );
+    } catch (e) {
+      if (e instanceof jwt.TokenExpiredError) {
+        throw new jwt.TokenExpiredError(
+          `Access Token is expired! - ${accessToken}`,
+          e.expiredAt,
+        );
+      }
+
+      throw new UnauthorizedException(e);
+    }
+  }
+
+  async verifyRefreshToken(refreshToken: string): Promise<UserSessionDto> {
+    try {
+      const decodedJwt = this.decodeToken(refreshToken);
+
+      const userSessionDto =
+        await this.userAuthRepository.getUserSessionDtoByUid(
+          CryptoUtils.decryptPostPK(decodedJwt['uid'], this.config.pkSecretKey),
+        );
+
+      if (refreshToken !== userSessionDto.refreshToken) {
+        throw new UnauthorizedException(
+          `Refresh Token does not match! - [${refreshToken}]`,
+        );
+      }
+
+      return userSessionDto;
+    } catch (e) {
+      if (e instanceof jwt.TokenExpiredError) {
+        throw new jwt.TokenExpiredError(
+          `Access Token is expired! - ${refreshToken}`,
+          e.expiredAt,
+        );
+      }
+
+      throw new UnauthorizedException(e);
+    }
+  }
+
+  async reissueJwtByUserSessionDto(
+    userSessionDto: UserSessionDto,
+  ): Promise<JwtDto> {
+    const decodedJwt = this.decodeToken(userSessionDto.refreshToken);
+    const expiresAt = decodedJwt.exp * 1000;
+
+    if (this.willExpire(expiresAt)) {
+      this.logger.info(`JWT has been reissued. - [${userSessionDto.uid}]`);
+
+      return await this.create(userSessionDto.uid, userSessionDto.userRole);
+    }
+
+    this.logger.info(
+      `Access Token has been reissued. - [${userSessionDto.uid}]`,
+    );
+
+    return new JwtDto(
+      this.generateToken(userSessionDto.uid, JwtService.ACCESS_TOKEN),
+      userSessionDto.refreshToken,
+    );
   }
 
   private generateToken(uid: string, tokenType: string): string {
@@ -48,6 +121,17 @@ export class JwtService {
         subject: uid,
         expiresIn: expiresIn,
       },
+    );
+  }
+
+  private decodeToken(token: string): jwt.JwtPayload {
+    return jwt.verify(token, this.config.jwtSecretKey) as jwt.JwtPayload;
+  }
+
+  private willExpire(expiresAt: number): boolean {
+    return (
+      expiresAt - new Date().getTime() <
+      Number(this.config.refreshTokenReissueTime)
     );
   }
 }
