82424e00d4b06e4e8f9100887690432d8341d43b
dev-msj <msj944@naver.com>
2023-12-05 07:48:37 +0900

    feat(user-auth.repository): getUserSessionDtoByUid 메서드 구현

--- personal-blog-service/src/user/repository/user-auth.repository.ts
+++ personal-blog-service/src/user/repository/user-auth.repository.ts
@@ -1,8 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserAuthEntity } from '../entities/user-auth.entity';
 import { DataSource, Repository } from 'typeorm';
 import { UserSessionDto } from '../dto/user-session.dto';
+import { TimeUtils } from '../../utils/time.utills';
 import { CacheIdUtils } from '../../utils/cache-id.utils';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
@@ -16,6 +17,25 @@ export class UserAuthRepository {
     private readonly userAuthRepository: Repository<UserAuthEntity>,
     private readonly dataSource: DataSource,
   ) {}
+
+  async getUserSessionDtoByUid(uid: string) {
+    return (
+      (await this.userAuthRepository
+        .createQueryBuilder('userAuthEntity')
+        .select('userAuthEntity.uid', 'uid')
+        .addSelect('userAuthEntity.refreshToken', 'refreshToken')
+        .addSelect('userAuthEntity.userRole', 'userRole')
+        .where({ uid: uid })
+        .cache(
+          CacheIdUtils.getUserSessionDtoCacheId(uid),
+          TimeUtils.getTicTimeHMS(6),
+        )
+        .getRawOne<UserSessionDto>()) ||
+      (() => {
+        throw new NotFoundException(`User does not exist! - [${uid}]`);
+      })()
+    );
+  }
 
   async updateUserAuthByUserSessionDto(userSessionDto: UserSessionDto) {
     this.dataSource.queryResultCache.remove([
