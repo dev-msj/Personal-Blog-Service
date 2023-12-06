bd2c1f2ef7b6d3dac1575e7886f441f95152b54d
dev-msj <msj944@naver.com>
2023-12-06 21:25:31 +0900

    feat: UserAuthDao 구현

---
+++ personal-blog-service/src/user/dao/user-auth.dao.ts
@@ -0,0 +1,34 @@
+import { UserRole } from '../../constant/user-role.enum';
+import { UserAuthEntity } from '../entities/user-auth.entity';
+import { UserAuthInterface } from './../dto/interface/user-auth.interface';
+
+export class UserAuthDao {
+  private uid: string;
+  private password: string;
+  private salt: string;
+  private socialYN: string;
+  private refreshToken: string;
+  private userRole: UserRole;
+
+  static from(userAuthInterface: UserAuthInterface): UserAuthDao {
+    const userAuthDao = new UserAuthDao();
+    userAuthDao.uid = userAuthInterface.uid;
+    userAuthDao.password = userAuthInterface.password;
+    userAuthDao.salt = userAuthInterface.salt;
+    userAuthDao.refreshToken = userAuthInterface.refreshToken;
+    userAuthDao.userRole = userAuthInterface.userRole;
+
+    return userAuthDao;
+  }
+
+  toUserAuthEntity(): UserAuthEntity {
+    return new UserAuthEntity(
+      this.uid,
+      this.password,
+      this.salt,
+      this.socialYN,
+      this.refreshToken,
+      this.userRole,
+    );
+  }
+}
