5b5267484b46e4a7de129f0ae06d864a09cf1b91
dev-msj <msj944@naver.com>
2023-12-07 00:01:17 +0900

    feat(user-auth.controller): login API 추가

--- personal-blog-service/src/user/controller/user-auth.controller.ts
+++ personal-blog-service/src/user/controller/user-auth.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserAuthRequestDto } from '../dto/user-auth-request.dto';
 import { UserAuthService } from './../service/user-auth.service';
+import { JwtDto } from '../dto/jwt.dto';
 
 @Controller('users/auth')
 export class UserAuthController {
   constructor(private readonly userAuthService: UserAuthService) {}
 
   @Post('join')
-  async join(@Body() userAuthRequestDto: UserAuthRequestDto) {
+  async join(@Body() userAuthRequestDto: UserAuthRequestDto): Promise<JwtDto> {
     return await this.userAuthService.createNewUser(userAuthRequestDto);
+  }
+
+  @Post('login')
+  async login(@Body() userAuthRequestDto: UserAuthRequestDto): Promise<JwtDto> {
+    return await this.userAuthService.login(userAuthRequestDto);
   }
 }
