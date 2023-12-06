8256d62c55a2ff789a5dae6f05340f6b465440c9
dev-msj <msj944@naver.com>
2023-12-06 21:58:48 +0900

    refactor(user.module): JwtService를 외부로 Exports

--- personal-blog-service/src/user/user.module.ts
+++ personal-blog-service/src/user/user.module.ts
@@ -6,8 +6,8 @@ import { UserInfoService } from './service/user-info.service';
 import { UserInfoRepository } from './repository/user-info.repository';
 import { UserAuthRepository } from './repository/user-auth.repository';
 import { UserAuthController } from './controller/user-auth.controller';
-import { JwtService } from '../auth/jwt.service';
 import { UserAuthService } from './service/user-auth.service';
+import { JwtService } from './service/jwt.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserInfoEntity, UserAuthEntity])],
@@ -19,5 +19,6 @@ import { UserAuthService } from './service/user-auth.service';
     UserAuthRepository,
     JwtService,
   ],
+  exports: [JwtService],
 })
 export class UserModule {}
