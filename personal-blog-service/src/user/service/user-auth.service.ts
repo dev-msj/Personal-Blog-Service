9a9adfbaf85efef6b57c9f10245506f399162fae
dev-msj <msj944@naver.com>
2023-12-07 20:15:38 +0900

    feat(user-auth.service): google oauth login 기능 구현

--- personal-blog-service/src/user/service/user-auth.service.ts
+++ personal-blog-service/src/user/service/user-auth.service.ts
@@ -9,12 +9,17 @@ import { SHA256 } from 'crypto-js';
 import { UserAuthDao } from './../dao/user-auth.dao';
 import { JwtService } from './jwt.service';
 import * as CryptoJS from 'crypto-js';
+import authConfig from '../../config/authConfig';
+import { ConfigType } from '@nestjs/config';
+import { LoginTicket, OAuth2Client } from 'google-auth-library';
 
 @Injectable()
 export class UserAuthService {
   constructor(
     @Inject(WINSTON_MODULE_PROVIDER)
     private readonly logger: Logger,
+    @Inject(authConfig.KEY)
+    private readonly config: ConfigType<typeof authConfig>,
     private readonly jwtService: JwtService,
     private readonly userAuthRepository: UserAuthRepository,
   ) {}
@@ -63,6 +68,32 @@ export class UserAuthService {
     return this.jwtService.create(userAuthDto.uid, userAuthDto.userRole);
   }
 
+  async googleOauthLogin(credentialToken: string): Promise<JwtDto> {
+    const ticket = await this.decodeCredentialToken(credentialToken);
+    const uid = ticket.getPayload().email;
+    const isExist = await this.userAuthRepository.isExist(uid);
+
+    // 인증된 토큰이나 UserAuth가 존재하지 않으면 생성시킨다.
+    if (!isExist) {
+      await this.userAuthRepository.saveUserAuthEntity(
+        UserAuthDao.from({
+          uid: uid,
+          password: '-',
+          salt: '-',
+          socialYN: 'Y',
+          refreshToken: '',
+          userRole: UserRole.USER,
+        }).toUserAuthEntity(),
+      );
+
+      this.logger.info(`A new social user has been created. - [${uid}]`);
+    }
+
+    const userAuthDto = await this.userAuthRepository.getUserAuthDto(uid);
+
+    return this.jwtService.create(userAuthDto.uid, userAuthDto.userRole);
+  }
+
   private hashingPassword(password: string, salt: string): string {
     for (let i = 0; i < 3; i++) {
       const strectchedPassword = `${password}${salt}`;
@@ -71,5 +102,21 @@ export class UserAuthService {
     }
 
     return password;
+  }
+
+  private async decodeCredentialToken(
+    credentialToken: string,
+  ): Promise<LoginTicket> {
+    try {
+      const client = new OAuth2Client(this.config.googleClientId);
+      return await client.verifyIdToken({
+        idToken: credentialToken,
+        audience: this.config.googleClientId,
+      });
+    } catch (e) {
+      this.logger.warn(`This token is not allowed. - [${credentialToken}]`);
+
+      throw new UnauthorizedException('This token is not allowed.');
+    }
   }
 }
