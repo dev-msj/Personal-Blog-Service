5f398f834d4b734f4dc0d8768bac78092f4d8c02
dev-msj <msj944@naver.com>
2023-12-07 20:10:40 +0900

    refactor(main): enableCors 옵션 적용

--- personal-blog-service/src/main.ts
+++ personal-blog-service/src/main.ts
@@ -5,6 +5,12 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.use(cookieParser());
 
   await app.listen(3000);
