90638a3f0c8e62858a3a388d8a9564dd65e36fd0
dev-msj <msj944@naver.com>
2023-12-09 04:53:21 +0900

    refactor(user-auth.controller): ApiOperation 및 return type 추가

--- personal-blog-service/src/user/controller/user-auth.controller.ts
+++ personal-blog-service/src/user/controller/user-auth.controller.ts
@@ -5,8 +5,10 @@ import { JwtDto } from '../dto/jwt.dto';
 import { Public } from '../../decorator/public.decorator';
 import {
   ApiBadRequestResponse,
+  ApiCreatedResponse,
   ApiInternalServerErrorResponse,
   ApiNotFoundResponse,
+  ApiOperation,
   ApiResponse,
   ApiTags,
   ApiUnauthorizedResponse,
@@ -20,9 +22,11 @@ export class UserAuthController {
   constructor(private readonly userAuthService: UserAuthService) {}
 
   @Post('join')
+  @ApiOperation({ description: '회원가입 요청 API' })
   @ApiResponse({
     status: 201,
     description: 'Response JWT(Access Token & Refresh Token)',
+    type: JwtDto,
   })
   @ApiInternalServerErrorResponse({
     description: 'User already exists. - [uid]',
@@ -35,9 +39,10 @@ export class UserAuthController {
   }
 
   @Post('login')
-  @ApiResponse({
-    status: 201,
+  @ApiOperation({ description: '로그인 요청 API' })
+  @ApiCreatedResponse({
     description: 'Response JWT(Access Token & Refresh Token)',
+    type: JwtDto,
   })
   @ApiNotFoundResponse({ description: 'User does not exist. - [uid]' })
   @ApiUnauthorizedResponse({ description: 'Password does not match.' })
@@ -49,9 +54,14 @@ export class UserAuthController {
   }
 
   @Post('oauth')
+  @ApiOperation({
+    description:
+      'Google Oauth으로 발급받은 Id Token으로 로그인 요청 API. 가입된 정보가 없을 경우 자동으로 회원가입한다.',
+  })
   @ApiResponse({
     status: 201,
     description: 'Response JWT(Access Token & Refresh Token)',
+    type: JwtDto,
   })
   @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
   @ApiBadRequestResponse({ description: 'Request body error' })
