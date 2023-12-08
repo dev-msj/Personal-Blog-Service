1458cb04ee5db3e1bc17aa40d8dc65efcf3dcb51
dev-msj <msj944@naver.com>
2023-12-09 03:58:31 +0900

    feat(main): swagger setup

--- personal-blog-service/src/main.ts
+++ personal-blog-service/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Personal Blog')
+    .setDescription('Backend service for personal blogging')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors({
     origin: true,
