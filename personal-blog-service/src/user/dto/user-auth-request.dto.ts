b5427e40fefb6839be09c54a970c392afec6015d
dev-msj <msj944@naver.com>
2023-12-09 04:52:08 +0900

    refactor(user-auth-request.dto): description 수정

--- personal-blog-service/src/user/dto/user-auth-request.dto.ts
+++ personal-blog-service/src/user/dto/user-auth-request.dto.ts
@@ -3,14 +3,13 @@ import { IsEmail, IsString } from 'class-validator';
 
 export class UserAuthRequestDto {
   @ApiProperty({
-    description: "User's email",
+    description: 'Email 형식의 string',
     example: 'example@email.com',
   })
   @IsEmail()
   readonly uid: string;
 
   @ApiProperty({
-    description: "User's password",
     example: 'example password',
   })
   @IsString()
