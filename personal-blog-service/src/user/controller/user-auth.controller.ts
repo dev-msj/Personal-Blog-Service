bd9f1b9ef0bd4244d14cf11d2053303640c3e1d3
dev-msj <msj944@naver.com>
2023-12-09 04:34:07 +0900

    feat(user-auth.controller): ApiReponse 추가

--- personal-blog-service/src/user/controller/user-auth.controller.ts
+++ personal-blog-service/src/user/controller/user-auth.controller.ts
@@ -3,13 +3,31 @@ import { UserAuthRequestDto } from '../dto/user-auth-request.dto';
 import { UserAuthService } from './../service/user-auth.service';
 import { JwtDto } from '../dto/jwt.dto';
 import { Public } from '../../decorator/public.decorator';
+import {
+  ApiBadRequestResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotAcceptableResponse,
+  ApiNotFoundResponse,
+  ApiResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @Public()
 @Controller('users/auth')
+@ApiTags('users/auth')
 export class UserAuthController {
   constructor(private readonly userAuthService: UserAuthService) {}
 
   @Post('join')
+  @ApiResponse({
+    status: 201,
+    description: 'Response JWT(Access Token & Refresh Token)',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'User already exists. - [uid]',
+  })
+  @ApiBadRequestResponse({ description: 'Request body error' })
   async join(
     @Body(ValidationPipe) userAuthRequestDto: UserAuthRequestDto,
   ): Promise<JwtDto> {
@@ -17,6 +35,13 @@ export class UserAuthController {
   }
 
   @Post('login')
+  @ApiResponse({
+    status: 201,
+    description: 'Response JWT(Access Token & Refresh Token)',
+  })
+  @ApiNotFoundResponse({ description: 'User does not exist. - [uid]' })
+  @ApiUnauthorizedResponse({ description: 'Password does not match.' })
+  @ApiBadRequestResponse({ description: 'Request body error' })
   async login(
     @Body(ValidationPipe) userAuthRequestDto: UserAuthRequestDto,
   ): Promise<JwtDto> {
@@ -24,6 +49,12 @@ export class UserAuthController {
   }
 
   @Post('oauth')
+  @ApiResponse({
+    status: 201,
+    description: 'Response JWT(Access Token & Refresh Token)',
+  })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
+  @ApiBadRequestResponse({ description: 'Request body error' })
   async googleOauthLogin(
     @Body('credentialToken') credentialToken: string,
   ): Promise<JwtDto> {
