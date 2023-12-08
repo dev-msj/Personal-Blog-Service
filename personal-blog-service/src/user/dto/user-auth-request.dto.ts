d3de98771169c10c64449d34b619e42050774b55
dev-msj <msj944@naver.com>
2023-12-09 04:31:50 +0900

    feat(user-auth-request.dto): ApiProperty 추가

--- personal-blog-service/src/user/dto/user-auth-request.dto.ts
+++ personal-blog-service/src/user/dto/user-auth-request.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class UserAuthRequestDto {
+  @ApiProperty({
+    description: "User's email",
+    example: 'example@email.com',
+  })
   @IsString()
   readonly uid: string;
 
+  @ApiProperty({
+    description: "User's password",
+    example: 'example password',
+  })
   @IsString()
   readonly password: string;
 
