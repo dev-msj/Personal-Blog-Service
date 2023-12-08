579766d62f16240b50fbe94cb29f08ef68cc437c
dev-msj <msj944@naver.com>
2023-12-08 22:25:21 +0900

    refactor(user-info.dto): IsNotEmpty validator 추가

--- personal-blog-service/src/user/dto/user-info.dto.ts
+++ personal-blog-service/src/user/dto/user-info.dto.ts
@@ -1,13 +1,16 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UserInfoDto {
   @IsString()
+  @IsNotEmpty()
   readonly uid: string;
 
   @IsString()
+  @IsNotEmpty()
   readonly nickname: string;
 
   @IsString()
+  @IsNotEmpty()
   readonly introduce: string;
 
   constructor(uid: string, nickname: string, introduce: string) {
