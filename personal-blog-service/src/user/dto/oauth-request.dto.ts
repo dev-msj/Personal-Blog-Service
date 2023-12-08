0d75d0a9993fb854b4fd47da293758675a5c6812
dev-msj <msj944@naver.com>
2023-12-09 04:38:33 +0900

    feat(oauth-request.dto): ApiProperty와 Validator 추가

--- personal-blog-service/src/user/dto/oauth-request.dto.ts
+++ personal-blog-service/src/user/dto/oauth-request.dto.ts
@@ -1,3 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
 export class OauthRequestDto {
+  @ApiProperty({
+    description: 'Google Oauth 완료 후 발급 받은 Id Token',
+  })
+  @IsString()
   readonly credentialToken: string;
 }
