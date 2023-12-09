b2098b7f0c1933e78ec3e7c9370044671804c561
dev-msj <msj944@naver.com>
2023-12-10 02:13:22 +0900

    feat: ApiProperty 추가

--- personal-blog-service/src/response/base-response.dto.ts
+++ personal-blog-service/src/response/base-response.dto.ts
@@ -1,7 +1,17 @@
 import { HttpStatus } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
 
 export abstract class BaseResponse {
+  @ApiProperty({
+    description: '상태 코드',
+    examples: ['200', '500', '...'],
+  })
   readonly code: number;
+
+  @ApiProperty({
+    description: '상태 메세지',
+    example: ['Success', 'Interal Server Error', '...'],
+  })
   readonly message: string;
 
   constructor(code: HttpStatus, message: string) {
--- personal-blog-service/src/user/dto/user-info-request.dto.ts
+++ personal-blog-service/src/user/dto/user-info-request.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UserInfoRequestDto {
+  @ApiProperty({
+    description: '중복되지 않는 닉네임',
+    example: 'exampleNickname',
+  })
   @IsString()
   @IsNotEmpty()
   readonly nickname: string;
 
+  @ApiProperty({
+    description: '간단한 자기 소개',
+    example: 'Hello World! This is example.',
+  })
   @IsString()
   @IsNotEmpty()
   readonly introduce: string;
