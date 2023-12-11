ba5325425c4a93afed7497ddc67e882b225d94e3
dev-msj <msj944@naver.com>
2023-12-11 09:46:21 +0900

    feat: ApiProperty 작성

--- personal-blog-service/src/blog/dto/pagination.dto.ts
+++ personal-blog-service/src/blog/dto/pagination.dto.ts
@@ -1,10 +1,14 @@
 import { IsArray } from 'class-validator';
 import { PaginationMetaDto } from './pagination-meta.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PaginationDto<T> {
   @IsArray()
   readonly data: T[];
 
+  @ApiProperty({
+    description: '페이징 정보',
+  })
   readonly paginationMeta: PaginationMetaDto;
 
   constructor(data: T[], paginationMetaDto: PaginationMetaDto) {
--- personal-blog-service/src/blog/dto/post.dto.ts
+++ personal-blog-service/src/blog/dto/post.dto.ts
@@ -1,24 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDate, IsNumber, IsString } from 'class-validator';
+import { CryptoUtils } from '../../utils/crypto.utils';
 
 export class PostDto {
+  @ApiProperty({ example: CryptoUtils.encryptPrimaryKey('1', 'example') })
   @IsString()
   readonly postId: string;
 
+  @ApiProperty({ example: 'example1@email.com' })
   @IsString()
   readonly postUid: string;
 
+  @ApiProperty({ example: 'Hello World!' })
   @IsString()
   readonly title: string;
 
+  @ApiProperty({ example: new Date() })
   @IsDate()
   readonly writeDatetime: Date;
 
+  @ApiProperty({ example: 'Example Contents.' })
   @IsString()
   readonly contents: string;
 
+  @ApiProperty({ example: 11 })
   @IsNumber()
   readonly hits: number;
 
+  @ApiProperty({ examples: ['nickname1', 'nickname2', 'nickname3'] })
   readonly postLikeUidList: string[];
 
   constructor(
