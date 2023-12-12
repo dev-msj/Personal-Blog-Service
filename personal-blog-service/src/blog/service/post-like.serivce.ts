406f3e7d6c9225f2db87691f6bddbfeb124ecbfa
dev-msj <msj944@naver.com>
2023-12-12 14:58:30 +0900

    refactor(post-like.service): postId를 decrypt하도록 수정

--- personal-blog-service/src/blog/service/post-like.serivce.ts
+++ personal-blog-service/src/blog/service/post-like.serivce.ts
@@ -1,17 +1,22 @@
 import { ConflictException, Inject, Injectable } from '@nestjs/common';
-import { UserInfoService } from '../../user/service/user-info.service';
-import { PostLikeDto } from '../dto/post-like.dto';
+import { ConfigType } from '@nestjs/config';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
+import { UserInfoService } from '../../user/service/user-info.service';
 import { PostLikeRepository } from '../repository/post-like.repository';
 import { PostLikeEntity } from '../entities/post-like.entity';
 import { PostLikeDao } from '../dao/post-like.dao';
+import { PostLikeDto } from '../dto/post-like.dto';
+import { CryptoUtils } from '../../utils/crypto.utils';
+import authConfig from '../../config/authConfig';
 
 @Injectable()
 export class PostLikeService {
   constructor(
     @Inject(WINSTON_MODULE_PROVIDER)
     private readonly logger: Logger,
+    @Inject(authConfig.KEY)
+    private readonly config: ConfigType<typeof authConfig>,
     private readonly postLikeRepository: PostLikeRepository,
     private readonly userInfoService: UserInfoService,
   ) {}
@@ -23,33 +28,39 @@ export class PostLikeService {
   }
 
   async addPostLikeUser(postLikeDto: PostLikeDto): Promise<void> {
-    const isExist = this.postLikeRepository.isExist(
-      PostLikeDao.from({ ...postLikeDto }).toPostLikeEntity(),
-    );
+    const postLikeEntity = this.getPostLikeEntity(postLikeDto);
+    const isExist = await this.postLikeRepository.isExist(postLikeEntity);
     if (isExist) {
       throw new ConflictException('PostId is already exist!');
     }
 
-    await this.postLikeRepository.savePostLikeEntity(
-      PostLikeDao.from({ ...postLikeDto }).toPostLikeEntity(),
-    );
+    await this.postLikeRepository.savePostLikeEntity(postLikeEntity);
 
     this.logger.info(`addPostLikeUser - [${JSON.stringify(postLikeDto)}]`);
   }
 
   async removePostLikeUser(postLikeDto: PostLikeDto): Promise<void> {
-    const isExist = this.postLikeRepository.isExist(
-      PostLikeDao.from({ ...postLikeDto }).toPostLikeEntity(),
-    );
+    const postLikeEntity = this.getPostLikeEntity(postLikeDto);
+    const isExist = this.postLikeRepository.isExist(postLikeEntity);
     if (!isExist) {
       throw new ConflictException('PostId does not exist!');
     }
 
-    await this.postLikeRepository.removePostLikeDto(
-      PostLikeDao.from({ ...postLikeDto }).toPostLikeEntity(),
-    );
+    await this.postLikeRepository.removePostLikeDto(postLikeEntity);
 
     this.logger.info(`removePostLikeUser - [${JSON.stringify(postLikeDto)}]`);
+  }
+
+  private getPostLikeEntity(postLikeDto: PostLikeDto): PostLikeEntity {
+    return PostLikeDao.from({
+      postId: Number(
+        CryptoUtils.decryptPrimaryKey(
+          postLikeDto.encryptedPostId,
+          this.config.pkSecretKey,
+        ),
+      ),
+      uid: postLikeDto.uid,
+    }).toPostLikeEntity();
   }
 
   private async getNicknameList(
