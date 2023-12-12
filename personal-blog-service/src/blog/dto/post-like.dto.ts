a460a42b0872dbc78c0dc6586185bcdb0bfb308d
dev-msj <msj944@naver.com>
2023-12-12 14:55:15 +0900

    refactor(post-like.dto): encryptedPostId를 받도록 변경

--- personal-blog-service/src/blog/dto/post-like.dto.ts
+++ personal-blog-service/src/blog/dto/post-like.dto.ts
@@ -1,14 +1,14 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class PostLikeDto {
-  @IsNumber()
-  readonly postId: number;
+  @IsString()
+  readonly encryptedPostId: string;
 
   @IsString()
   readonly uid: string;
 
-  constructor(postId: number, uid: string) {
-    this.postId = postId;
+  constructor(encryptedPostId: string, uid: string) {
+    this.encryptedPostId = encryptedPostId;
     this.uid = uid;
   }
 }
