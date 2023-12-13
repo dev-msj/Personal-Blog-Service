21a0ef6c2c8e10eb794801b29410b222e4a74f39
dev-msj <msj944@naver.com>
2023-12-13 11:38:43 +0900

    style(jwt.dto): ApiProperty option 추가

--- personal-blog-service/src/user/dto/jwt.dto.ts
+++ personal-blog-service/src/user/dto/jwt.dto.ts
@@ -1,10 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class JwtDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Access Token',
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhY2Nlc3NUb2tlbiJ9.BuXw-Oc1gM-NNF9-xGs1koroCqJnd_rl0e-7pyDMDJI',
+  })
   readonly accessToken: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Refresh Token',
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJyZWZyZXNoVG9rZW4ifQ.cZymiL0u4IzKC8w66RVrxFGbRHdQho6p6BSVRO7YkZ8',
+  })
   readonly refreshToken: string;
 
   constructor(accessToken: string, refreshToken: string) {
