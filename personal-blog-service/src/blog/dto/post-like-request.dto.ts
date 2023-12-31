fecde5d64c4eb1e4e5582d9dcfd90517b8691176
dev-msj <msj944@naver.com>
2023-12-31 22:19:20 +0900

    refactor(post-like-request.dto): description을 더 자세하게 작성

--- personal-blog-service/src/blog/dto/post-like-request.dto.ts
+++ personal-blog-service/src/blog/dto/post-like-request.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString } from 'class-validator';
 
 export class PostLikeRequestDto {
   @ApiProperty({
-    description: '암호화된 Post Id',
+    description: 'Server에서 암호화하여 보내준 postId 값',
     example: 'U2FsdGVkX19IAexd2B4rK0T64%2F8L15kuzw1w78zX4sI%3D',
   })
   @IsString()
