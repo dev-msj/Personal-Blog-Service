fd5d4de07bdbf99f6b5c21cd536847cbb6e5eeb1
dev-msj <msj944@naver.com>
2023-12-31 22:24:17 +0900

    refactor(pagination-meta.dto): 각 property들의 description 작성

--- personal-blog-service/src/blog/dto/pagination-meta.dto.ts
+++ personal-blog-service/src/blog/dto/pagination-meta.dto.ts
@@ -1,19 +1,34 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class PaginationMetaDto {
-  @ApiProperty({ example: 3 })
+  @ApiProperty({
+    description: '총 페이지 수',
+    example: 3,
+  })
   readonly total: number;
 
-  @ApiProperty({ example: 1 })
+  @ApiProperty({
+    description: '현재 페이지 번호',
+    example: 1,
+  })
   readonly currentPage: number;
 
-  @ApiProperty({ example: 3 })
+  @ApiProperty({
+    description: '마지막 페이지 번호',
+    example: 3,
+  })
   readonly lastPage: number;
 
-  @ApiProperty({ example: false })
+  @ApiProperty({
+    description: '이전 페이지 존재 여부',
+    example: false,
+  })
   readonly hasPreviousPage: boolean;
 
-  @ApiProperty({ example: true })
+  @ApiProperty({
+    description: '다음 페이지 존재 여부',
+    example: true,
+  })
   readonly hasNextPage: boolean;
 
   constructor(total: number, currentPage: number, lastPage: number) {
