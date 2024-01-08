6f7d95fce4fb3e3e9998c7334971960d1f2ff90e
dev-msj <msj944@naver.com>
2024-01-08 19:27:35 +0900

    refactor(user-info.service): getUserInfoByUid 수정
    암호화된 uid를 복호화하여 select하도록 변경

--- personal-blog-service/src/user/service/user-info.service.ts
+++ personal-blog-service/src/user/service/user-info.service.ts
@@ -4,12 +4,17 @@ import { UserInfoRepository } from '../repository/user-info.repository';
 import { UserInfoDao } from '../dao/user-info.dao';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
+import authConfig from '../../config/authConfig';
+import { ConfigType } from '@nestjs/config';
+import { CryptoUtils } from '../../utils/crypto.utils';
 
 @Injectable()
 export class UserInfoService {
   constructor(
     @Inject(WINSTON_MODULE_PROVIDER)
     private readonly logger: Logger,
+    @Inject(authConfig.KEY)
+    private readonly config: ConfigType<typeof authConfig>,
     private readonly userInfoRepository: UserInfoRepository,
   ) {}
 
@@ -29,9 +34,14 @@ export class UserInfoService {
   }
 
   async getUserInfoByUid(uid: string): Promise<UserInfoDto> {
-    return UserInfoDao.from(
-      await this.userInfoRepository.findUserInfoEntity(uid),
-    ).toUserInfoDto();
+    return UserInfoDao.from({
+      ...(await this.userInfoRepository.findUserInfoEntity(
+        CryptoUtils.decryptPrimaryKey(uid, this.config.pkSecretKey),
+      )),
+      // findUserInfoEntity의 결과를 암호화하지 않고, 요청받은 값을 재사용한다.
+      // 클라이언트 입장에서 uid값이 변경되어 다른 uid로 혼동하게 될 것을 방지하기 위함.
+      uid: uid,
+    }).toUserInfoDto();
   }
 
   async updateUserInfo(userInfoDto: UserInfoDto): Promise<void> {
