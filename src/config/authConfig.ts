a48084a06ea45de2028467b31a4db72d80ce66c0
dev-msj <msj944@naver.com>
2025-09-20 02:24:59 +0900

    fix: jwt 타입 에러 해결
    이전 버전에서는 jwt.sign을 할 때 토큰의 타입을 그냥 받아도 문제가 없었다.
    하지만 업데이트한 버전에서는 StringValue 또는 number 타입을 요구한다.
    이에 맞게 env 값을 로드할 때 타입을 검증 및 명시적으로 로드하여 해결한다.

--- src/config/authConfig.ts
+++ src/config/authConfig.ts
@@ -1,11 +1,15 @@
 import { registerAs } from '@nestjs/config';
+import type { StringValue } from 'ms';
 
 export default registerAs('config', () => ({
   pkSecretKey: process.env.PK_SECRET_KEY,
   jwtSecretKey: process.env.JWT_SECRET_KEY,
   jwtIssuer: process.env.JWT_ISSUER,
-  accessTokenExpireTime: process.env.JWT_ACCESSTOKEN_EXPIRE_TIME,
-  refreshTokenExpireTime: process.env.JWT_REFRESHTOKEN_EXPIRE_TIME,
-  refreshTokenReissueTime: process.env.JWT_REFRESHTOKEN_REISSUE_TIME,
+  accessTokenExpireTime:
+    (process.env.JWT_ACCESSTOKEN_EXPIRE_TIME as StringValue | number) || '1h',
+  refreshTokenExpireTime:
+    (process.env.JWT_REFRESHTOKEN_EXPIRE_TIME as StringValue | number) || '30d',
+  refreshTokenReissueTime:
+    (process.env.JWT_REFRESHTOKEN_REISSUE_TIME as StringValue | number) || '3d',
   googleClientId: process.env.GOOGLE_CLIENT_ID,
 }));
--- src/config/validationEnv.ts
+++ src/config/validationEnv.ts
@@ -4,8 +4,25 @@ export const validationEnv = Joi.object({
   PK_SECRET_KEY: Joi.string().required().length(16),
   JWT_SECRET_KEY: Joi.string().required(),
   JWT_ISSUER: Joi.string().required(),
-  JWT_ACCESSTOKEN_EXPIRE_TIME: Joi.string().required(),
-  JWT_REFRESHTOKEN_EXPIRE_TIME: Joi.string().required(),
-  JWT_REFRESHTOKEN_REISSUE_TIME: Joi.number().required(),
+  // expires 관련 값을 '1h', '7d' 같은 StringValue나 number 타입만 허용
+  JWT_ACCESSTOKEN_EXPIRE_TIME: Joi.alternatives()
+    .try(
+      Joi.string().pattern(/^\d+[smhd]$|^\d+$/),
+      Joi.number().integer().positive(),
+    )
+    .required(),
+
+  JWT_REFRESHTOKEN_EXPIRE_TIME: Joi.alternatives()
+    .try(
+      Joi.string().pattern(/^\d+[smhd]$|^\d+$/),
+      Joi.number().integer().positive(),
+    )
+    .required(),
+  JWT_REFRESHTOKEN_REISSUE_TIME: Joi.alternatives()
+    .try(
+      Joi.string().pattern(/^\d+[smhd]$|^\d+$/),
+      Joi.number().integer().positive(),
+    )
+    .required(),
   GOOGLE_CLIENT_ID: Joi.string().required(),
 });
