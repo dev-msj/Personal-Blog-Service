eda2b36b7f6b184d0dbfdcdbb4c5b1d5ff604deb
dev-msj <msj944@naver.com>
2025-10-03 23:31:45 +0900

    fix(create-post.dto): 누락된 검증 추가

--- src/blog/dto/create-post.dto.ts
+++ src/blog/dto/create-post.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString } from 'class-validator';
 import { PostDto } from './post.dto';
 import { PickType } from '@nestjs/mapped-types';
 
-export class CreatePostDto extends PickType(PostDto, ['title', 'contents']) {}
+export class CreatePostDto extends PickType(PostDto, ['title', 'contents']) {
+  @IsString()
+  @IsNotEmpty()
+  readonly title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly contents: string;
+}
