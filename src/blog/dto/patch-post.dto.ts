1f33d6b2c0c7054f60f200276fe4da51a72e6b20
dev-msj <msj944@naver.com>
2025-10-13 17:57:14 +0900

    refactor(patch-post.dto): 속성 검증 추가

--- src/blog/dto/patch-post.dto.ts
+++ src/blog/dto/patch-post.dto.ts
@@ -1,8 +1,20 @@
-import { PickType } from '@nestjs/swagger';
+import { IntersectionType, PartialType, PickType } from '@nestjs/swagger';
 import { PostDto } from './post.dto';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
-export class PatchPostDto extends PickType(PostDto, [
-  'postId',
-  'title',
-  'contents',
-]) {}
+export class PatchPostDto extends IntersectionType(
+  PickType(PostDto, ['postId']),
+  PartialType(PickType(PostDto, ['title', 'contents'])),
+) {
+  @IsString()
+  @IsNotEmpty()
+  readonly postId: string;
+
+  @IsString()
+  @IsOptional()
+  readonly title?: string;
+
+  @IsString()
+  @IsOptional()
+  readonly contents?: string;
+}
