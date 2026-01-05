fb63059800255c925ff89c74ebf442ec8c02b5c5
dev-msj <msj944@naver.com>
2026-01-05 23:49:39 +0900

    refactor(entity): Entity 필드의 readonly 접근 제한자 제거
    TypeORM Entity는 update 작업을 위해 mutable해야 함.
    readonly 제거로 TypeORM 컨벤션 준수 및 유지보수성, 테스트 용이성 향상.
    
    Closes #37

--- src/user/entities/user-info.entity.ts
+++ src/user/entities/user-info.entity.ts
@@ -14,29 +14,29 @@ import { UserAuthEntity } from './user-auth.entity';
 @Unique(['nickname'])
 export class UserInfoEntity {
   @PrimaryColumn({ name: 'UID', length: 100 })
-  readonly uid: string;
+  uid: string;
 
   @Column({ name: 'NICKNAME', length: 100 })
-  readonly nickname: string;
+  nickname: string;
 
   @Column({ name: 'INTRODUCE', length: 500 })
-  readonly introduce: string;
+  introduce: string;
 
   @CreateDateColumn({
     name: 'CREATE_DATETIME',
     type: 'datetime',
   })
-  readonly createDatetime: Date;
+  createDatetime: Date;
 
   @UpdateDateColumn({
     name: 'MODIFY_DATETIME',
     type: 'datetime',
   })
-  readonly modifyDatetime: Date;
+  modifyDatetime: Date;
 
   @OneToOne(() => UserAuthEntity, (userAuthEntity) => userAuthEntity.uid)
   @JoinColumn({ name: 'UID', referencedColumnName: 'uid' })
-  readonly userAuthEntity: UserAuthEntity;
+  userAuthEntity: UserAuthEntity;
 
   constructor(uid: string, nickname: string, introduce: string) {
     this.uid = uid;
