5db4a00646f9afbabbde175b31ab43bcf73eb025
Alex Moon <msj944@naver.com>
2026-02-27 04:25:06 +0900

    refactor(response): FailureResponse 생성자의 code 파라미터 타입을 HttpStatus | ErrorCode로 수정 (#65)
    BaseExceptionFilter에서 ErrorCode 타입의 exception.errorCode를
    FailureResponse에 전달하지만, 생성자 타입이 HttpStatus로만 선언되어 있어
    실제 사용 패턴과 타입 정의가 불일치했다. HttpStatus | ErrorCode 유니언 타입으로
    수정하여 타입 안전성을 확보한다.
    
    #58

--- src/response/base-response.dto.ts
+++ src/response/base-response.dto.ts
@@ -1,10 +1,11 @@
 import { HttpStatus } from '@nestjs/common';
+import { ErrorCode } from '../constant/error-code.enum';
 
 export abstract class BaseResponse {
   readonly code: number;
   readonly message: string;
 
-  constructor(code: HttpStatus, message: string) {
+  constructor(code: HttpStatus | ErrorCode, message: string) {
     this.code = code;
     this.message = message;
   }
